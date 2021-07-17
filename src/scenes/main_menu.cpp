bbc4a939f7d46fe37bfb53dd3a32aedd3b8c6097
Lautaro Coccia <lautaroauditore@gmail.com>
2021-07-16 21:20:12 -0300

    Add main menu scene

---
+++ src/scenes/main_menu.cpp
@@ -0,0 +1,33 @@
+#include "main_menu.h"
+
+#include "raylib.h"
+
+Texture2D buttonTexture;
+main_menu::main_menu()
+{
+}
+main_menu::~main_menu()
+{
+	
+}
+void main_menu::Start()
+{
+	buttonTexture = LoadTexture("Raw/UI/Mouse/buttonLarge.png");
+
+}
+void main_menu::Update()
+{
+	
+}
+void main_menu::Draw()
+{
+	DrawText("This is main menu!", 310, 200, 20, BLACK);
+	
+	DrawTexture(buttonTexture, 300 , 160 - buttonTexture.height / 2, WHITE);
+	DrawText("PLAY", 375, 150, 20, BLACK);
+
+}
+void main_menu::Deinitialization()
+{
+
+}
---
+++ src/scenes/main_menu.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+class main_menu
+{
+public:
+	main_menu();
+	~main_menu();
+	void Start();
+	void Update();
+	void Draw();
+	void Deinitialization();
+};
+#endif // !MAIN_MENU_H
