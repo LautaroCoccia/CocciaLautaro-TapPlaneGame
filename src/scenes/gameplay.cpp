0d5919781b27473afe96424f89be7084cef4adf9
Lautaro Coccia <lautaroauditore@gmail.com>
2021-07-18 17:03:49 -0300

    Add obstacles on gameplay scene

--- src/scenes/gameplay.cpp
+++ src/scenes/gameplay.cpp
@@ -3,30 +3,37 @@
 #include "raylib.h"
 
 #include "objects/player.h"
+#include "objects/obstacles.h"
 
 player* player1;
+obstacles* obs;
+
 gameplay::gameplay()
 {
 	player1 = new player();
+	obs = new obstacles();
 }
 gameplay::~gameplay()
 {
+	delete obs;
 	delete player1;
 }
 void gameplay::Start()
 {
 	player1->Start();
+	obs->Start();
 }
 void gameplay::Update()
 {
 	player1->Input();
 	player1->Update();
+	obs->Update();
 }
 void gameplay::Draw() 
 {
 	DrawText("This is game scene!", 310, 200, 20, BLACK);
 	player1->Draw();
-
+	obs->Draw();
 }
 void gameplay::Deinitialization()
 {
