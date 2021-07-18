d381db35a6eb27dc69b487c09874b95cd098df7c
Lautaro Coccia <lautaroauditore@gmail.com>
2021-07-17 23:47:07 -0300

    Update  player movement

--- src/objects/player.cpp
+++ src/objects/player.cpp
@@ -4,14 +4,12 @@
 Vector2 position;
 
 Rectangle playerVisual;
+
+int FloatToInt(float value);
+
 player::player()
 {
-	//position.x = static_cast<float>(GetScreenWidth() / 2);
-	position.y = static_cast<float>(GetScreenHeight() / 2 - 20);
-	playerVisual.x = position.x;
-	playerVisual.y = position.y;
-	playerVisual.height = 40;
-	playerVisual.width = 40;
+	
 }
 int movementUp = 0;
 player::~player()
@@ -20,7 +18,14 @@ player::~player()
 }
 void player::Start() 
 {
+	playerVisual.x = position.x;
+	playerVisual.y = position.y;
 
+	playerVisual.height = 40;
+	playerVisual.width = 40;
+
+	position.x = static_cast<float>(GetScreenWidth() / 2);
+	position.y = static_cast<float>(GetScreenHeight() / 4 - playerVisual.height / 2);
 }
 void player::Input()
 {
@@ -31,12 +36,16 @@ void player::Input()
 }
 void player::Update()
 {
-	if (position.y +40 < GetScreenHeight())
+	if (position.y + 40 < GetScreenHeight()) 
 	{
 		position.y  += 175 * GetFrameTime() ;
 	}
 }
 void player::Draw()
 {
-	DrawRectangle(GetScreenWidth() / 2 - 20 , static_cast<int>(position.y), 40 , 40, RED);
+	DrawRectangle(GetScreenWidth() / 2 - 20 , FloatToInt(position.y), FloatToInt(playerVisual.width), FloatToInt(playerVisual.height), RED);
+}
+int FloatToInt(float value)
+{
+	return static_cast<int>(value);
 }
